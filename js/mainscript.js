989b5020b893ead0681de478ab4186589a0b483f
Brian Lusina <lusinabrian@gmail.com>
2016-07-26 12:49:09 +0300

    store button containers in variables

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 	/*Obtain all JS calculator buttons and store in variables*/
+	
 	//calculator display
 	var calcDisplay = $('#calc-display');
 	//buttons
 	var acButton = $('#ac-btn'), cButton = $('#cancel-btn'), lftBracketbtn = $('#left-bracket'), rghtBracketbtn = $('#right-bracket-btn'), sevBtn = $('#sev-btn'), eghtBtn = $('#eight-btn'), nineBtn = $('#nine-btn'), plusBtn = $('#plus-btn'), fourBtn = $('#four-btn'), fiveBtn = $('#five-btn'), sixBtn = $('#six-btn'), minusBtn = $('#minu-btn'), oneBtn = $('#one-btn'), twoBtn = $('#two-btn'), threeBtn = ('#three-btn'), multBtn = $('#mult-btn'), periodBtn = $('#period-btn'), zeroBtn = $('#zero-btn'), doubleZero = $('#double-zero-btn'), divBtn = $('#div-btn');
+	
+	/*equals button*/
+	var equalsBtn = $('#equals-btn');
+	
+	//record click events from button containers
+	var buttonContainer = $('.button-container');
+	var equalsContainer = $('.eq-container');
+	
 	
 });
