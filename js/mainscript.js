96328babec16449efbb4f8e2f509774be3155e23
Brian Lusina <lusinabrian@gmail.com>
2016-07-26 12:45:11 +0300

    Add calculator buttons to variables

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-	//TODO: insert functions
-
+	/*Obtain all JS calculator buttons and store in variables*/
+	//calculator display
+	var calcDisplay = $('#calc-display');
+	//buttons
+	var acButton = $('#ac-btn'), cButton = $('#cancel-btn'), lftBracketbtn = $('#left-bracket'), rghtBracketbtn = $('#right-bracket-btn'), sevBtn = $('#sev-btn'), eghtBtn = $('#eight-btn'), nineBtn = $('#nine-btn'), plusBtn = $('#plus-btn'), fourBtn = $('#four-btn'), fiveBtn = $('#five-btn'), sixBtn = $('#six-btn'), minusBtn = $('#minu-btn'), oneBtn = $('#one-btn'), twoBtn = $('#two-btn'), threeBtn = ('#three-btn'), multBtn = $('#mult-btn'), periodBtn = $('#period-btn'), zeroBtn = $('#zero-btn'), doubleZero = $('#double-zero-btn'), divBtn = $('#div-btn');
+	
 });
