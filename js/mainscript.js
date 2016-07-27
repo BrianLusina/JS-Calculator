d183c02d84bef71f191e585bd880048aaf61f31e
Brian Lusina <lusinabrian@gmail.com>
2016-07-27 11:04:08 +0300

    configured clear buttons

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){
-	/*Obtain all JS calculator buttons and store in variables*/
+	/*Obtain all JS calculator buttons and store in variables
+	*/
 	
-	//calculator display
-	var calcDisplay = $('#calc-display p');
-	//buttons
-	var acButton = $('#ac-btn'), cButton = $('#cancel-btn'), lftBracketbtn = $('#left-bracket'), rghtBracketbtn = $('#right-bracket-btn'), sevBtn = $('#sev-btn'), eghtBtn = $('#eight-btn'), nineBtn = $('#nine-btn'), plusBtn = $('#plus-btn'), fourBtn = $('#four-btn'), fiveBtn = $('#five-btn'), sixBtn = $('#six-btn'), minusBtn = $('#minu-btn'), oneBtn = $('#one-btn'), twoBtn = $('#two-btn'), threeBtn = ('#three-btn'), multBtn = $('#mult-btn'), periodBtn = $('#period-btn'), zeroBtn = $('#zero-btn'), doubleZero = $('#double-zero-btn'), divBtn = $('#div-btn');
-	
-	/*equals button*/
-	var equalsBtn = $('#equals-btn');
-	
+	/*button containers*/
 	var buttonContainer = $('.button-container');
 	var clearButton = $('.clear-button');
 	var equalsContainer = $('.eq-container');
+	
+	//calculator display
+	var calcDisplay = $('#calc-display p');
+	
+	//clear buttons
+	var acButton = $('#ac-btn'), 
+		cButton = $('#cancel-btn');
+	
+	//bracket buttons
+	var lftBracketbtn = $('#left-bracket'),
+		rghtBracketbtn = $('#right-bracket-btn');
+		
+	//number buttons
+	var sevBtn = $('#sev-btn'), eghtBtn = $('#eight-btn'), nineBtn = $('#nine-btn'), fourBtn = $('#four-btn'), fiveBtn = $('#five-btn'), sixBtn = $('#six-btn'), oneBtn = $('#one-btn'), twoBtn = $('#two-btn'), threeBtn = ('#three-btn'), periodBtn = $('#period-btn'), zeroBtn = $('#zero-btn'), doubleZero = $('#double-zero-btn');
+	
+	//operator buttons
+	var operators = $('.operator-button'),
+		plusBtn = $('#plus-btn'),
+		minusBtn = $('#minu-btn'),
+		multBtn = $('#mult-btn'),
+		divBtn = $('#div-btn');
+	
+	/*equals button*/
+	var equalsBtn = $('#equals-btn');
 	
 	/*record click events from button containers
 	store value in variable and display in calc display
@@ -23,18 +41,16 @@ $(document).ready(function(){
 		
 		console.log(currVal);
 		calcDisplay.append("<span>" + currVal + "</span>");
-		
 	});
 	
-	
-	//configure AC button to clear display
+	//configure AC button to clear display	
 	acButton.on('click', function(){
-		calcDisplay.empty();
+	   calcDisplay.empty();		
 	});
 	
-	//remove last child input from display
+	 //remove last child input from display		
 	cButton.on('click', function(){
-		$("#calc-display p:last-child").empty();
-	});
+		$("#calc-display p span:last-child").remove(); 
+	});	
 	
 });
