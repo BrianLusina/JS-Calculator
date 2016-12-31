37a9342bf0d8b9fce0b07e4eeb99255f852ebd3c
BrianLusina <lusinabrian@gmail.com>
2016-12-31 08:44:17 +0300

    import styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { render } from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
+import './styles/reset.css';
+import './styles/font-awesome/css/font-awesome.min.css'
+import './styles/foundation.min.css';
 
 render(<App/>, document.getElementById('app'))
